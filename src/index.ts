f46b5d983ac145fcfa2feeaa68af7d2877db7e41
Alexandre Milani <alexandre.milani00@gmail.com>
2020-11-11 22:35:24 -0400

    Added Electron into project.
    Electron added with ts-node and typescript.

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from "electron"
+import * as path from "path"
+
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile(path.join(__dirname, '../src/screens/main.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
